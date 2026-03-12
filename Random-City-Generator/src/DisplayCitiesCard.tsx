3ef9fffdf895e779e16c097bebd68f6cf467724f
Rajdeep5270 <rajdeepex5270@gmail.com>
2026-03-12 18:02:10 +0530

    PR-1

--- Random-City-Generator/src/DisplayCitiesCard.tsx
+++
@@ -1,5 +0,0 @@
-export default function DisplayCitiesCard() {
-    return <>
-        
-    </>
-}
--- Random-City-Generator/src/Footer.tsx
+++
@@ -1,5 +0,0 @@
-export default function Footer() {
-    return <>
-        <p className="text-light text-center py-3 bg-secondary mb-0">All Rights Reserved 2026.</p>
-    </>
-}
